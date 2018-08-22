dc9c2bbf8ec908c2b46660c1ce399174297e2a56
mustafich <mustafichpikap@gmail.com>
2018-08-22 18:46:02 +0300

    1

---
+++ js/es6/build/common.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
+
+}());
+
+//# sourceMappingURL=common.js.map
---
+++ js/es6/common.js
@@ -0,0 +1,8 @@
+"use strict";
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
